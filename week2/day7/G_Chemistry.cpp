f1b566bab8e06ef9f9f941f528588e90a04ba1e4
Md. Shakhawat Hossen <shakhawat09@students.diu.ac>
2024-11-09 23:57:30 +0600

    Completed: week2 day7 three practice problem

---
+++ week2/day7/G_Chemistry.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+
+        map<char, int> mp;
+        for (char c : s)
+        {
+            mp[c]++;
+        }
+
+        int oddCount = 0;
+
+        for (auto [x, y] : mp)
+        {
+            if (y % 2 != 0)
+                oddCount++;
+        }
+
+        if (oddCount - 1 < 0)
+            oddCount = 0;
+        else
+            oddCount--;
+
+        if (k >= oddCount)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    return 0;
+}
