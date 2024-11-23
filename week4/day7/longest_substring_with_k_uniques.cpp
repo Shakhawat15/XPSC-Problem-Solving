872a44efbd8e3e3a29dd74dee6dd1feb432a4ff2
Md. Shakhawat Hossen <shakhawat09@students.diu.ac>
2024-11-23 23:34:59 +0600

    Completed: week4 day7 three practice problem with remaining 1

---
+++ week4/day7/longest_substring_with_k_uniques.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    int longestKSubstr(string &s, int k)
+    {
+        map<char, int> mp;
+        int result = -1, l = 0, r = 0;
+        while (r < s.size())
+        {
+            mp[s[r]]++;
+            if (mp.size() == k)
+                result = max(result, r - l + 1);
+            while (mp.size() > k)
+            {
+                mp[s[l]]--;
+                if (mp[s[l]] == 0)
+                    mp.erase(s[l]);
+                l++;
+            }
+            r++;
+        }
+        return result;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int k;
+        cin >> k;
+        Solution ob;
+        cout << ob.longestKSubstr(s, k) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+}
+
+// } Driver Code Ends
